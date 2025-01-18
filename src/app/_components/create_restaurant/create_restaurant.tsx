08237f61fb1e4f94b3b2a72e43be543a3cf9c9fa
Luís <luisaugusto75@gmail.com>
2025-01-18 08:14:32 -0300

    Atualizado criação de restaurante

--- src/app/_components/create_restaurant/create_restaurant.tsx
+++ src/app/_components/create_restaurant/create_restaurant.tsx
@@ -12,30 +12,38 @@ export default function Cadastro() {
 
   return (
     <main className={styles.main}>
-        <div className={styles.container}>
-            <h1 className={styles.heading}>Criar restaurante</h1>
-            <div className={styles.dividerOu}>
-
-            </div>
-            <form>
-              <p className={styles.nome}>Insira o nome do restaurante</p>
-                <div className={styles.usuario}>
-                    <input type="text" name="restaurante" id="restaurante" placeholder="" autoComplete="off" />
-                </div>
-              <p className={styles.nome}>Insira a localização do restaurante</p>
-                <div className={styles.senha}>
-                    <input type="text" name="localizacao" id="localizacao" placeholder="" autoComplete="off" />
-                </div>
-                <p className={styles.nome}>Insira a descrição do restaurante</p>
-                <div className={styles.senha}>
-                    <input type="text" name="descricao" id="descricao" placeholder="" autoComplete="off" />
-                </div>
-                <div className={styles.entrar}>
-                    <input type="submit" value="Criar"/>
-                </div>
-            </form>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Criar restaurante</h1>
+        <div className={styles.dividerOu}>
 
         </div>
+        <form>
+          <p className={styles.nome}>Escreva o nome do restaurante.</p>
+          <div className={styles.usuario}>
+            <input type="text" name="restaurante" id="restaurante" placeholder="" autoComplete="off" />
+          </div>
+          <p className={styles.nome}>Escreva as categorias do restaurante.</p>
+          <div className={styles.usuario}>
+            <input type="text" name="catergoria" id="categoria" placeholder="" autoComplete="off" />
+          </div>
+          <p className={styles.nome}>Escreva a faixa de preço do restaurante.</p>
+          <div className={styles.usuario}>
+            <input type="text" name="faixa_de_preco" id="faixa_de_preco" placeholder="" autoComplete="off" />
+          </div>
+          <p className={styles.nome}>Escreva a descrição do restaurante:</p>
+          <div className={styles.descricao}>
+            <textarea name="descricao" id="descricao" placeholder="Descrição do restaurante" autoComplete="off"></textarea>
+          </div>
+          <p className={styles.nome}>Escreva o cardápio do restaurante:</p>
+          <div className={styles.descricao}>
+            <textarea name="cardapio" id="cardapio" placeholder="Menu do restaurante" autoComplete="off"></textarea>
+          </div>
+          <div className={styles.entrar}>
+            <input type="submit" value="Criar" />
+          </div>
+        </form>
+
+      </div>
     </main>
   );
 };
